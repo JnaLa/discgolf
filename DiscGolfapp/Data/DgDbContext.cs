65e0c87fedd8de15502d5d711d1ee91fc792e430
Joona Laitila <joona.laitila123@hotmail.com>
2022-06-09 13:21:30 +0300

    Groundwork

---
+++ DiscGolfapp/Data/DgDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscGolfapp.Data
+{
+    public class DgDbContext : DbContext
+    {
+        
+        public DgDbContext(DbContextOptions<DgDbContext> options) : base(options)
+        {
+
+        }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int Score { get; set; }
+    }
+}
